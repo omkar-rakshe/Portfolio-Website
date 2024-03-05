70519742238921ec5b985f57fbb2e9c0afe78f8f
Omkar Rakshe <omkarrakshe98@gmail.com>
2024-03-05 18:59:05 +0530

    Modifying better css

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -24,7 +24,8 @@ const Skills = () => {
                                 I have proficiency in following
                             </h1> */}
                             <p> {/*className="text-lg" */}
-                            Proficient in a diverse array of programming languages and technologies, with a focus on both frontend and backend development for web applications. Skilled in harnessing contemporary frameworks and tools to craft immersive user interfaces. Possesses adept problem-solving abilities, strong communication skills, and a collaborative mindset, driven by a intense dedication to ongoing learning and industry evolution.
+                            Proficient in a diverse array of programming languages and technologies, with a focus on both frontend and backend development for web applications.
+                            Skilled in harnessing contemporary frameworks and tools to craft immersive user interfaces. Possesses adept problem-solving abilities, strong communication skills, and a collaborative mindset, driven by a intense dedication to ongoing learning and industry evolution.
                             </p>
                         {/* Button if you want add like hire me or resume */}
                         </div>
@@ -32,7 +33,7 @@ const Skills = () => {
                     <div className="flex justify-center">
                         {/* Skills */} 
                         {/* space-x-2 */}
-                        <div className="flex space-x-5 space-y-2 w-2/2 h-full items-center  flex-wrap">
+                        <div className="flex  space-x-5 space-y-3 w-2/2 h-fit items-center flex-wrap">
                             <p className="bg-violet-600 text-white rounded-full mt-3 w-fit px-4 py-3 cursor-pointer hover:bg-violet-800 "><i className="fa-brands fa-html5 "></i> HTML</p>
                             <p className="bg-violet-600 text-white rounded-full mt-3 w-fit px-4 py-3 cursor-pointer hover:bg-violet-800"><i className="fa-brands fa-css3"></i> CSS,Tailwind CSS</p>
                             <p className="bg-violet-600 text-white rounded-full mt-3 w-fit px-4 py-3 cursor-pointer hover:bg-violet-800"><i className="fa-brands fa-js"></i> Javascript</p>
